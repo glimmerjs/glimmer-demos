ab2384a5fa4b294253b7cf09626c604ef9c335de
Jarek Radosz <jradosz@gmail.com>
2017-06-10 15:26:48 +0200

    Add rewritten db-monster demo.
    Original version: https://github.com/glimmerjs/glimmer-vm/blob/0932ad7c6d23f88cf1558df28c95542ddd858c14/packages/glimmer-demos/lib/dbmon.ts

--- src/ui/components/glimmer-demos/component.ts
+++ src/ui/components/glimmer-demos/component.ts
@@ -2,7 +2,7 @@ import Component, { tracked } from "@glimmer/component";
 
 export default class GlimmerDemos extends Component {
   @tracked currentDemo;
-  demos = ['uptime-boxes', 'ripples-demo'];
+  demos = ['uptime-boxes', 'ripples-demo', 'db-monster'];
 
   setCurrentDemo(demo) {
     this.currentDemo = demo;
