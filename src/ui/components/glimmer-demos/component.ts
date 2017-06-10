3d85b22f88f053826809a6d5facebf5fb9a1b24c
Jarek Radosz <jradosz@gmail.com>
2017-06-10 13:37:44 +0200

    Add rewritten ripples demo.
    Original version: https://github.com/glimmerjs/glimmer-vm/blob/0932ad7c6d23f88cf1558df28c95542ddd858c14/packages/glimmer-demos/lib/ripples.ts

--- src/ui/components/glimmer-demos/component.ts
+++ src/ui/components/glimmer-demos/component.ts
@@ -2,7 +2,7 @@ import Component, { tracked } from "@glimmer/component";
 
 export default class GlimmerDemos extends Component {
   @tracked currentDemo;
-  demos = ['uptime-boxes'];
+  demos = ['uptime-boxes', 'ripples-demo'];
 
   setCurrentDemo(demo) {
     this.currentDemo = demo;
---
+++ src/ui/components/ripples-demo/component.ts
@@ -0,0 +1,63 @@
+import Component, { tracked } from '@glimmer/component';
+
+interface Point {
+  timestamp: number;
+  opacity: number;
+  size: number;
+  x: number;
+  y: number;
+}
+
+export default class RipplesDemo extends Component {
+  @tracked width = window.innerWidth;
+  @tracked height = window.innerHeight;
+  @tracked count = 0;
+  @tracked points: Point[] = [];
+
+  constructor(options) {
+    super(options);
+
+    window.addEventListener('resize', this.windowResizeHandler);
+    document.addEventListener('mousemove', this.mouseMoveHandler);
+
+    this.tick();
+  }
+
+  willDestroy() {
+    document.removeEventListener('mousemove', this.mouseMoveHandler);
+    window.removeEventListener('resize', this.windowResizeHandler);
+  }
+
+  windowResizeHandler = () => {
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+  }
+
+  mouseMoveHandler = (event: MouseEvent) => {
+    this.points = [ ...this.points, {
+      timestamp: window.performance.now(),
+      opacity: 1.0,
+      size: 0.0,
+      x: event.layerX,
+      y: event.layerY,
+    }];
+  }
+
+  tick = () => {
+    let timestamp = window.performance.now();
+
+    this.points = this.points.map((point) => {
+      let delta = timestamp - point.timestamp;
+      let opacity = Math.exp(-delta / 250);
+      let size = Math.max(delta / 10, 0);
+
+      if (opacity > 0.001) {
+        return { ...point, opacity, size };
+      }
+    }).filter(v => v);
+
+    this.count = this.points.length;
+
+    window.requestAnimationFrame(this.tick);
+  }
+}
